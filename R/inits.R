59d37bc32f45b780f03a9b05c9779c84061934d1
Vince Carey <stvjc@channing.harvard.edu>
2008-04-11 17:02:21 +0000

    dynamic export support
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/GGdata@626 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/inits.R
@@ -0,0 +1,19 @@
+
+.onLoad = function(libname, pkgname) {
+ cat(paste(pkgname, "loading...\n"))
+ require("ncdf", quietly=TRUE)
+ ncpatt = ".*\\.nc$"
+ allncdf = dir(system.file("extdata", package=pkgname), patt=ncpatt,
+    full=TRUE)
+ allsuff = dir(system.file("extdata", package=pkgname), patt=ncpatt)
+ for (i in 1:length(allsuff)) {
+   obname = paste(pkgname, allsuff[i], sep="_")
+   AnnotationDbi:::addToNamespaceAndExport(obname, open.ncdf(allncdf[i]),
+       pkgname)
+   }
+}
+
+.onUnload <- function(libpath) {
+ obs = objects("package:GGdata", patt=".*\\.nc$")
+ sapply(obs, close.ncdf)
+}
